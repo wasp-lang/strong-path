b92682c682711a8487e65f41c967c215115abb14
Alexander Pankoff <ccntrq@screenri.de>
2022-02-03 17:17:51 +0100

    Add note about possibilty of obtaining the same hash

--- src/StrongPath/Instances.hs
+++ src/StrongPath/Instances.hs
@@ -11,5 +11,10 @@ import StrongPath.Types
 -- and StrongPath.Internal. (This cycle would arise due to the use of
 -- `toFilePath` from FilePath in the instance declaration and the dependency of
 -- the FilePath module on the types from the Internal module)
+
+-- |
+-- Caveat: For two relative Paths, that only differ in the Directory, that they
+-- are relative to, this Hashable instance will return the same hash even though
+-- they are different paths.
 instance Hashable (Path s b t) where
   hashWithSalt salt = hashWithSalt salt . toFilePath
