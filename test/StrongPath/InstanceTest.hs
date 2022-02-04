f1652bb73c31119d90e143a92060b592a595e2a8
Alexander Pankoff <ccntrq@screenri.de>
2022-02-04 11:12:47 +0100

    Rework tests for Hashable instance
    The previous tests were not working on windows and tested an
    implementation detail of the Hashable instance. The new tests should be
    portable across systems and don't rely on any internals of the hashable
    implementation anymore.
    
    See suggestions by @Martinsos:
    https://github.com/wasp-lang/strong-path/pull/44#issuecomment-1029344295

--- test/StrongPath/InstanceTest.hs
+++ test/StrongPath/InstanceTest.hs
@@ -8,7 +8,12 @@ import Test.Tasty.Hspec (testSpec)
 
 test_StrongPathInstance :: IO TestTree
 test_StrongPathInstance = testSpec "StrongPath.Instance" $ do
-  it "Hashable returns hash of underlying filepath" $ do
-    let rawPath = "relPath/dir/"
-    path <- parseRelDir rawPath
-    hash rawPath `shouldBe` hash path
+  it "Different paths have different hash" $ do
+    aPath <- parseRelDir "a"
+    bPath <- parseRelDir "b"
+    hash aPath `shouldNotBe` hash bPath
+  it "Concatenated Paths have same hash as Path directly constructed from parts" $ do
+    aPath <- parseRelDir "a"
+    bPath <- parseRelDir "b"
+    abPath <- parseRelDir "a/b"
+    hash (aPath </> bPath) `shouldBe` hash abPath
