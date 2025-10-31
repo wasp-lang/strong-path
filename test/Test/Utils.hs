02182c66bb938a29cc926fa26f680e6adeb4bd34
Filip Sodić <filip.sodic@gmail.com>
2025-10-31 15:48:24 +0100

    Support GHC range from 9.0.2 to 9.12.2 (#54)

--- test/Test/Utils.hs
+++ test/Test/Utils.hs
@@ -13,16 +13,17 @@ systemFpRoot = if FP.pathSeparator == '\\' then "C:\\" else "/"
 
 -- | Takes posix path and converts it into windows path if running on Windows or leaves as it is if on Unix.
 posixToSystemFp :: FilePath -> FilePath
-posixToSystemFp posixFp = maybeSystemRoot ++ systemFpRootless
-  where
-    maybeSystemRoot = if head posixFp == '/' then systemFpRoot else ""
-    posixFpRootless = if head posixFp == '/' then tail posixFp else posixFp
-    systemFpRootless = map (\c -> if c == '/' then FP.pathSeparator else c) posixFpRootless
+posixToSystemFp = convertPosixFp systemFpRoot FP.pathSeparator
 
 -- | Takes posix path and converts it into windows path.
 posixToWindowsFp :: FilePath -> FilePath
-posixToWindowsFp posixFp = maybeWinRoot ++ winFpRootless
+posixToWindowsFp = convertPosixFp "C:\\" FPW.pathSeparator
+
+convertPosixFp :: FilePath -> Char -> FilePath -> FilePath
+convertPosixFp newRoot newSeparator posixFp =
+  case posixFp of
+    "" -> error "Empty string is not a valid posix path."
+    '/' : restOfAbsPosixFp -> newRoot ++ map convertSeparator restOfAbsPosixFp
+    relPosixFp -> map convertSeparator relPosixFp
   where
-    maybeWinRoot = if head posixFp == '/' then "C:\\" else ""
-    posixFpRootless = if head posixFp == '/' then tail posixFp else posixFp
-    winFpRootless = map (\c -> if c == '/' then FPW.pathSeparator else c) posixFpRootless
+    convertSeparator c = if c == '/' then newSeparator else c
